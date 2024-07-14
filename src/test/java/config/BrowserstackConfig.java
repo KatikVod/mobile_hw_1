a488b1a8889dff00969664abe77e6001de4c9a2a
Ekaterina Vodolazhskaia <katik_v@inbox.ru>
2024-07-14 21:00:08 +0300

    added Config.LoadPolicy

--- src/test/java/config/BrowserstackConfig.java
+++ src/test/java/config/BrowserstackConfig.java
@@ -2,8 +2,9 @@ package config;
 
 import org.aeonbits.owner.Config;
 
+@Config.LoadPolicy(Config.LoadType.FIRST)
 @Config.Sources({
-        // "classpath:${env}.properties",
+        "classpath:${env}.properties",
         "classpath:android.properties"
 })
 public interface BrowserstackConfig extends Config {
